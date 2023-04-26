d9ebf956ed5014688d94b241868a6c6ff0dcc109
NeilKleistGao <neilkleist@gmail.com>
2023-04-26 22:46:39 +0800

    WIP: Add config files

--- build.sh
+++ build.sh
@@ -7,4 +7,8 @@ pacman -Sy --needed sudo archiso --noconfirm
 cp -rf /usr/share/archiso/configs/releng ./profile
 mkdir /run/shm
 
+# copy config files
+cp /dandelinux/profiledef.sh ./profile/profiledef.sh
+cp /dandelinux/pacman.conf ./profile/pacman.conf
+
 sudo mkarchiso -o /output -v ./profile
