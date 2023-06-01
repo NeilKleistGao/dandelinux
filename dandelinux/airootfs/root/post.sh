1e857ff1b006ce6192a8239e8d1fbe852f9e33ae
NeilKleistGao <neilkleist@gmail.com>
2023-06-01 23:03:56 +0800

    Add more packages

--- dandelinux/airootfs/root/post.sh
+++ dandelinux/airootfs/root/post.sh
@@ -3,6 +3,9 @@
 pacman -Syu && pacman -S archlinuxcn-keyring
 pacman -Sy yay --noconfirm
 
+# some packages need confirmation
+yay -S visual-studio-code-bin
+
 cat software.x86_64 | while read line
 do
   if [[ $line == \#* ]]
@@ -13,3 +16,13 @@ do
     yay -Sy $line --noconfirm
   fi
 done
+
+touch ~/.xprofile
+echo "export GTK_IM_MODULE=fcitx" >> ~/.xprofile
+echo "export QT_IM_MODULE=fcitx" >> ~/.xprofile
+echo "export XMODIFIERS=@im=fcitx" >> ~/.xprofile
+
+chsh -s /usr/bin/zsh
+sh -c "$(curl -fssL https://raw,github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# TODO: dotfiles
