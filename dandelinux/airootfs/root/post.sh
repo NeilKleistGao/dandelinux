f3b883516aea57585097ac4620bba0282a78c99e
NeilKleistGao <neilkleist@gmail.com>
2023-06-02 23:04:02 +0800

    Fix several problems

--- dandelinux/airootfs/root/post.sh
+++ dandelinux/airootfs/root/post.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-pacman -Syu && pacman -S archlinuxcn-keyring
-pacman -Sy yay --noconfirm
+sudo pacman -Syu && sudo pacman -S archlinuxcn-keyring
+sudo pacman -Sy yay --noconfirm
 
-# some packages need confirmation
-yay -S visual-studio-code-bin
+# some packages need no `sudo`
+yay -S visual-studio-code-bin --noconfirm
+yay -S iflyime --noconfirm
+yay -S qcef --noconfirm
+yay -S google-chrome --noconfirm
+yay -S godot-mono --noconfirm
 
 cat software.x86_64 | while read line
 do
@@ -13,16 +17,16 @@ do
     echo $line
   else
     echo "installing" $line
-    yay -Sy $line --noconfirm
+    sudo yay -S $line --noconfirm
   fi
 done
 
 touch ~/.xprofile
 echo "export GTK_IM_MODULE=fcitx" >> ~/.xprofile
 echo "export QT_IM_MODULE=fcitx" >> ~/.xprofile
-echo "export XMODIFIERS=@im=fcitx" >> ~/.xprofile
+echo "export XMODIFIERS=\"@im=fcitx\"" >> ~/.xprofile
 
 chsh -s /usr/bin/zsh
-sh -c "$(curl -fssL https://raw,github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fssL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # TODO: dotfiles
