7c0af1c32a14a217d837fb32eb00fa366e451fdd
NeilKleistGao <neilkleist@gmail.com>
2023-06-02 23:27:01 +0800

    Fix again and add dotfiles

--- dandelinux/airootfs/root/post.sh
+++ dandelinux/airootfs/root/post.sh
@@ -3,13 +3,6 @@
 sudo pacman -Syu && sudo pacman -S archlinuxcn-keyring
 sudo pacman -Sy yay --noconfirm
 
-# some packages need no `sudo`
-yay -S visual-studio-code-bin --noconfirm
-yay -S iflyime --noconfirm
-yay -S qcef --noconfirm
-yay -S google-chrome --noconfirm
-yay -S godot-mono --noconfirm
-
 cat software.x86_64 | while read line
 do
   if [[ $line == \#* ]]
@@ -17,7 +10,7 @@ do
     echo $line
   else
     echo "installing" $line
-    sudo yay -S $line --noconfirm
+    yay -S $line --noconfirm
   fi
 done
 
